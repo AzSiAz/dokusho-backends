cd1d9215b15908480153b6c689f0dd74ab54e7d6
AzSiAz <stephan@deumier.dev>
2025-08-24 16:32:18 +0200

    implement missing query for `SourceApi` trait

--- apps/api/src/graphql/schema/sources/query.rs
+++ apps/api/src/graphql/schema/sources/query.rs
@@ -1,9 +1,12 @@
 use async_graphql::{Context, Object, Result};
-use dokusho_core::{SourceApi, SourceId};
+use dokusho_core::{SourceApi, SourceId, SourceSerieChapterId, SourceSerieId};
 
 use crate::graphql::{AuthGuard, GraphQLContext};
 
-use super::types::{GraphQLFetchSearchSerieFilter, GraphQLPaginatedSmallSerie, GraphQLSource};
+use super::types::{
+    GraphQLChapters, GraphQLFetchSearchSerieFilter, GraphQLPaginatedSmallSerie, GraphQLSerie,
+    GraphQLSerieChapterData, GraphQLSource,
+};
 
 #[derive(Default)]
 pub struct SourcesQuery;
@@ -82,6 +85,63 @@ impl SourcesQuery {
 
         source
             .fetch_search_serie(page, filters.into())
+            .await
+            .map_err(|e| async_graphql::Error::new(e.to_string()))
+            .map(|data| data.into())
+    }
+
+    #[graphql(guard = "AuthGuard")]
+    async fn source_serie(
+        &self,
+        ctx: &Context<'_>,
+        #[graphql(name = "source_id")] source_id: SourceId,
+        #[graphql(name = "serie_id")] serie_id: SourceSerieId,
+    ) -> Result<Option<GraphQLSerie>> {
+        let context = ctx.data::<GraphQLContext>()?;
+        let source = context.sources.get_source(&source_id).ok_or_else(|| {
+            async_graphql::Error::new(format!("Source '{}' not found", source_id))
+        })?;
+
+        match source.fetch_serie_detail(serie_id).await {
+            Ok(serie) => Ok(Some(serie.into())),
+            Err(_) => Ok(None),
+        }
+    }
+
+    #[graphql(guard = "AuthGuard")]
+    async fn source_serie_chapters(
+        &self,
+        ctx: &Context<'_>,
+        #[graphql(name = "source_id")] source_id: SourceId,
+        #[graphql(name = "serie_id")] serie_id: SourceSerieId,
+    ) -> Result<GraphQLChapters> {
+        let context = ctx.data::<GraphQLContext>()?;
+        let source = context.sources.get_source(&source_id).ok_or_else(|| {
+            async_graphql::Error::new(format!("Source '{}' not found", source_id))
+        })?;
+
+        source
+            .fetch_serie_chapters(serie_id)
+            .await
+            .map_err(|e| async_graphql::Error::new(e.to_string()))
+            .map(|data| data.into())
+    }
+
+    #[graphql(guard = "AuthGuard")]
+    async fn source_serie_chapters_data(
+        &self,
+        ctx: &Context<'_>,
+        #[graphql(name = "source_id")] source_id: SourceId,
+        #[graphql(name = "serie_id")] serie_id: SourceSerieId,
+        #[graphql(name = "chapter_id")] chapter_id: SourceSerieChapterId,
+    ) -> Result<GraphQLSerieChapterData> {
+        let context = ctx.data::<GraphQLContext>()?;
+        let source = context.sources.get_source(&source_id).ok_or_else(|| {
+            async_graphql::Error::new(format!("Source '{}' not found", source_id))
+        })?;
+
+        source
+            .fetch_chapter_data(serie_id, chapter_id)
             .await
             .map_err(|e| async_graphql::Error::new(e.to_string()))
             .map(|data| data.into())
